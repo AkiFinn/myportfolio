63b7504e3f7be557bec47288095799f85090064a
Junel Timon <blitearagon@gmail.com>
2025-06-08 13:32:03 +0800

    Third commit

--- src/App.js
+++ src/App.js
@@ -1,18 +1,15 @@
-
-
-import Hero from './components/Hero';
-import About from './components/About';
-import Skills from './components/Skills';
-import Contact from './components/Contact';
-import Footer from './components/Footer';
-import Logo from './components/img/dev_logo.png';
-import './styles.css';
-
+import Hero from "./components/Hero";
+import About from "./components/About";
+import Skills from "./components/Skills";
+import Contact from "./components/Contact";
+import Footer from "./components/Footer";
+import Logo from "./components/img/dev_logo.png";
+import "./styles.css";
 function App() {
   return (
     <div className="App">
       <nav className="p-2" class="navbar">
-          <img src={Logo} alt="logo" />
+        <img src={Logo} alt="logo" />
       </nav>
       <main>
         <Hero />
@@ -20,7 +17,7 @@ function App() {
         <Skills />
         <Contact />
       </main>
-            <Footer />
+      <Footer />
     </div>
   );
 }
